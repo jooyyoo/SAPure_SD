a2336f48d72bff7b18ffb85e51fff1db42840e84
jooyyoo <aliceyun9@naver.com>
2024-11-19 22:00:43 +0900

    [feat] #2 정수기 이미지 선택 시 효과 주기
    deploy 했을 때 sidenavigation 안뜸

--- sapure_sd_fert_view/webapp/controller/FertView.controller.js
+++ sapure_sd_fert_view/webapp/controller/FertView.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
 ],
     function (Controller, JSONModel) {
         "use strict";
@@ -10,24 +10,32 @@ sap.ui.define([
 
                 var oModel = new JSONModel({
                     expanded: true // 사이드 네비게이션의 기본 상태
-                  });
-                  this.getView().setModel(oModel, "expand");
-                // var oModel = new sap.ui.model.odata.v2.ODataModel("/sap/opu/odata/sap/ZC503SDCDS0001_CDS/");
-                // this.getView().setModel(oModel);
+                });
+                this.getView().setModel(oModel, "expand");
 
             },
 
-            // onCollapseExpandPress() {
-            //     const oSideNavigation = this.byId("sideNavigation"),
-            //         bExpanded = oSideNavigation.getExpanded();
-
-            //     oSideNavigation.setExpanded(!bExpanded);
-            // },
-
-            onCollapseExpandPress() {
+            onCollapseExpandPress: function () {
                 var oModel = this.getView().getModel("expand");
                 var bExpanded = oModel.getProperty("/expanded"); // boolean 값
                 oModel.setProperty("/expanded", !bExpanded); // expanded 상태 토글 
+            },
+
+            onPressMatnr: function (oEvent) {
+
+                console.log("Press 이벤트 발생:", oEvent);
+                var oSelectedItem = oEvent.getSource();
+                var oContext = oSelectedItem.getBindingContext();
+
+                console.log("BindingContext:", oContext); // 컨텍스트 정보 출력
+
+                if (oContext) {
+                    var sMatnr = oContext.getProperty("Matnr");
+                    console.log("Matnr 값: ", sMatnr);  // Matnr 값이 잘 출력되는지 확인
+                    sap.m.MessageToast.show("제품 번호: " + sMatnr);
+                } else {
+                    console.log("선택된 항목이 없습니다.");
+                }
             },
         });
     });
