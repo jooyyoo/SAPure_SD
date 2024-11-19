f13f2b153ebc8d3bfea05e68075ab139d45d657a
jooyyoo <aliceyun9@naver.com>
2024-11-19 18:28:59 +0900

    [feat] 정수기 판매 페이지 화면 2차 개발

--- sapure_sd_fert_view/webapp/controller/FertView.controller.js
+++ sapure_sd_fert_view/webapp/controller/FertView.controller.js
@@ -1,12 +1,33 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
-function (Controller) {
-    "use strict";
+    function (Controller, JSONModel) {
+        "use strict";
 
-    return Controller.extend("zc503sd.gw0001.sapuresdfertview.controller.FertView", {
-        onInit: function () {
+        return Controller.extend("zc503sd.gw0001.sapuresdfertview.controller.FertView", {
+            onInit: function () {
 
-        }
+                var oModel = new JSONModel({
+                    expanded: true // 사이드 네비게이션의 기본 상태
+                  });
+                  this.getView().setModel(oModel, "expand");
+                // var oModel = new sap.ui.model.odata.v2.ODataModel("/sap/opu/odata/sap/ZC503SDCDS0001_CDS/");
+                // this.getView().setModel(oModel);
+
+            },
+
+            // onCollapseExpandPress() {
+            //     const oSideNavigation = this.byId("sideNavigation"),
+            //         bExpanded = oSideNavigation.getExpanded();
+
+            //     oSideNavigation.setExpanded(!bExpanded);
+            // },
+
+            onCollapseExpandPress() {
+                var oModel = this.getView().getModel("expand");
+                var bExpanded = oModel.getProperty("/expanded"); // boolean 값
+                oModel.setProperty("/expanded", !bExpanded); // expanded 상태 토글 
+            },
+        });
     });
-});
