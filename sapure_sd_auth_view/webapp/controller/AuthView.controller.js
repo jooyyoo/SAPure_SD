8dfa1536821e66d9e670acabf37918c8d05128fd
jooyyoo <aliceyun9@naver.com>
2024-11-24 23:32:03 +0900

    [chore] 코드 정렬 및 불필요한 주석 제거

--- sapure_sd_auth_view/webapp/controller/AuthView.controller.js
+++ sapure_sd_auth_view/webapp/controller/AuthView.controller.js
@@ -23,7 +23,7 @@ sap.ui.define([
 
                 //서버에서 해당 아이디를 가진 고객을 read
                 let oModel = new sap.ui.model.odata.v2.ODataModel("/sap/opu/odata/sap/ZC503SDGW0001_SRV/");
-                
+
                 console.log(oModel);
 
                 var id = this.getView().byId('Iid').getValue();
@@ -48,51 +48,47 @@ sap.ui.define([
                 var password = sap.ui.getCore().byId("DI_pw").getValue();
                 var passwordConfirm = sap.ui.getCore().byId("DI_rpw").getValue();
 
-                
+
                 if (password !== passwordConfirm) {
                     sap.m.MessageToast.show("비밀번호가 일치하지 않습니다.");
                     return;
                 }
-            
+
                 // 서버로 전송하는 로직 (예시)F
                 let oModel = this.getView().getModel();
-                
+
                 var oUpdate = {
-                    Customer : username,
-                    Email : email,
+                    Customer: username,
+                    Email: email,
                     Password: password,
                 };
 
-                // alert("/AuthSet(Customer='" + oUpdate.Customer + "')");
-
                 oModel.update
-                (
-                    "/AuthSet(Customer='" + oUpdate.Customer + "')", 
-                    oUpdate, 
-                    {
-                        success: function () 
+                    (
+                        "/AuthSet(Customer='" + oUpdate.Customer + "')",
+                        oUpdate,
                         {
-                            // 서버 응답 성공 시 처리
-                            
-                            // oModel.refresh()
-                            sap.m.MessageToast.show("비밀번호 재설정이 완료되었습니다.");
-                            this.oResizableDialog.close();
-                        }.bind(this),
-                        error: function (oError) 
-                        {
-                            // 서버 응답 실패 시 처리
-                            console.log(oUpdate.username);
-                            sap.m.MessageToast.show("서버 오류가 발생했습니다. 다시 시도해주세요.");
+                            success: function () {
+                                // 서버 응답 성공 시 처리
+
+                                // oModel.refresh()
+                                sap.m.MessageToast.show("비밀번호 재설정이 완료되었습니다.");
+                                this.oResizableDialog.close();
+                            }.bind(this),
+                            error: function (oError) {
+                                // 서버 응답 실패 시 처리
+                                console.log(oUpdate.username);
+                                sap.m.MessageToast.show("서버 오류가 발생했습니다. 다시 시도해주세요.");
+                            }
                         }
-                    }
-                );
+                    );
             },
 
             onLinkPress: function () {
 
                 if (!this.oResizableDialog) {
                     this.oResizableDialog = new sap.m.Dialog({
-                        id : "reDialog",
+                        id: "reDialog",
                         title: "비밀번호 재설정",
                         contentWidth: "450px",
                         contentHeight: "auto",
